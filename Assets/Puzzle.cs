cf7e83d148a65990cf63ce39ea6ad17102d6829e
dkjk1234 <103244178+Yoonsuji@users.noreply.github.com>
2023-10-31 14:32:07 +0900

    feat: UI

--- Assets/Puzzle.cs
+++ Assets/Puzzle.cs
@@ -1,10 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 
 /*
  i:  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14
